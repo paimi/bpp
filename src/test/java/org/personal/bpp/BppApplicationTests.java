59a3a0bf5e3bebb1ed599dbb3a943ee02e300f95
Paimi <parizmendi@coppel.com>
2020-01-29 12:19:16 -0700

    Agregando proyecto principal sin clases ni configuraciones iniciales

---
+++ src/test/java/org/personal/bpp/BppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.personal.bpp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
